181351c1e094fbcfa2c07eddf706909b0306d67c
softeca-adrian-roman <adrian.roman@softeca.es>
2026-03-13 14:45:03 +0100

    feat: Route web middleware

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,59 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\VendedorController;
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\VehiculoController;
+use App\Http\Controllers\ClienteVehiculoController;
+use Spatie\Permission\Middleware\PermissionMiddleware;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+require __DIR__.'/auth.php';
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth'])->group(function () {
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
-require __DIR__.'/auth.php';
+    Route::resource('clientes', ClienteController::class)
+        ->middleware([
+            PermissionMiddleware::class . ':ver clientes',
+            PermissionMiddleware::class . ':crear clientes',
+            PermissionMiddleware::class . ':editar clientes',
+            PermissionMiddleware::class . ':eliminar clientes',
+        ]);
+
+
+    Route::resource('vehiculos', VehiculoController::class)
+        ->middleware([
+            PermissionMiddleware::class . ':ver vehiculos',
+            PermissionMiddleware::class . ':crear vehiculos',
+            PermissionMiddleware::class . ':editar vehiculos',
+            PermissionMiddleware::class . ':eliminar vehiculos',
+        ]);
+
+
+    Route::post('/clientes/{cliente}/vehiculos', [ClienteVehiculoController::class, 'store'])
+        ->name('clientes.vehiculos.store')
+        ->middleware(PermissionMiddleware::class . ':asignar vehiculos');
+
+    Route::delete('/clientes/{cliente}/vehiculos/{vehiculo}', [ClienteVehiculoController::class, 'destroy'])
+        ->name('clientes.vehiculos.destroy')
+        ->middleware(PermissionMiddleware::class . ':asignar vehiculos');
+
+    Route::resource('vendedores', VendedorController::class)->except('show')
+        ->middleware([
+            PermissionMiddleware::class . ':ver vendedores',
+            PermissionMiddleware::class . ':crear vendedores',
+            PermissionMiddleware::class . ':editar vendedores',
+            PermissionMiddleware::class . ':eliminar vendedores',
+        ]);
+    Route::get('/dashboard', function () {
+    return view('dashboard');
+    })->name('dashboard');
+});
