93487c14a31d34cfa3ea6de9b108d15319ba8876
softeca-adrian-roman <adrian.roman@softeca.es>
2026-03-23 09:14:10 +0100

    refactor: 'la plataforma'-'IMS'

--- app/Mail/VendedorWelcomeMail.php
+++ app/Mail/VendedorWelcomeMail.php
@@ -29,7 +29,7 @@ class VendedorWelcomeMail extends Mailable
     public function envelope(): Envelope
     {
         return new Envelope(
-            subject: 'Bienvenido a la plataforma - Nuevo vendedor',
+            subject: 'Bienvenido a IMS - Nuevo vendedor',
         );
     }
 
